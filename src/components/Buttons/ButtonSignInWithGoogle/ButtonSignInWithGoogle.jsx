e17dc75f77a02a9caad7cb44e14b064400b49015
Nejca13 <nicolascontreras677@gmail.com>
2025-01-08 16:22:14 -0300

    Refactor Google Sign-In button component to improve conditional rendering and maintainability

--- src/components/Buttons/ButtonSignInWithGoogle/ButtonSignInWithGoogle.jsx
+++ src/components/Buttons/ButtonSignInWithGoogle/ButtonSignInWithGoogle.jsx
@@ -10,40 +10,41 @@ const ButtonSignInWithGoogle = () => {
   const [auth, setAuth] = useAuth()
   const router = useRouter()
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.google) {
-      window.handleCredentialResponse = (response) => {
-        const responsePayload = jwtDecode(response.credential)
-        if (responsePayload.sub) {
-          setAuth(responsePayload)
-          router.push('/google-auth')
-        }
+    '4e7921ab61fa598a'
+    window.handleCredentialResponse = (response) => {
+      const responsePayload = jwtDecode(response.credential)
+      if (responsePayload.sub) {
+        setAuth(responsePayload)
+        router.push('/google-auth')
       }
     }
   }, [])
 
   return (
-    <>
-      <Script src='https://accounts.google.com/gsi/client' async defer />
-      <div
-        id='g_id_onload'
-        data-client_id='714395374113-539b12soro38d2srslfjgt07l04m8j4a.apps.googleusercontent.com'
-        data-context='signup'
-        data-ux_mode='popup'
-        data-callback='handleCredentialResponse'
-        /* data-auto_select='true' */
-        data-itp_support='true'
-      ></div>
-      <div
-        className='g_id_signin'
-        data-type='standard'
-        data-shape='pill'
-        data-theme='outline'
-        data-text='signin_with'
-        data-size='large'
-        data-locale='es'
-        data-logo_alignment='left'
-      ></div>
-    </>
+    typeof window !== 'undefined' && (
+      <>
+        <Script src='https://accounts.google.com/gsi/client' async defer />
+        <div
+          id='g_id_onload'
+          data-client_id='714395374113-539b12soro38d2srslfjgt07l04m8j4a.apps.googleusercontent.com'
+          data-context='signup'
+          data-ux_mode='popup'
+          data-callback='handleCredentialResponse'
+          /* data-auto_select='true' */
+          data-itp_support='true'
+        ></div>
+        <div
+          className='g_id_signin'
+          data-type='standard'
+          data-shape='pill'
+          data-theme='outline'
+          data-text='signin_with'
+          data-size='large'
+          data-locale='es'
+          data-logo_alignment='left'
+        ></div>
+      </>
+    )
   )
 }
 
