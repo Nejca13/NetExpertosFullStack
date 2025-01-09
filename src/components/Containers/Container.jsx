5ab39a22a76e9ebb856fd0253144fac979e41036
Nejca13 <nicolascontreras677@gmail.com>
2025-01-09 11:11:15 -0300

    Refactor geolocation handling in Container component to check for stored user location before requesting new coordinates

--- src/components/Containers/Container.jsx
+++ src/components/Containers/Container.jsx
@@ -3,22 +3,27 @@ import styles from './Container.module.css'
 const Container = ({ children, justifyContent }) => {
   // Solicitar el uso de la API de geolocalización
   const useGeolocation = () => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log('seteando nueva ubicacion')
-        const { latitude, longitude } = position.coords
-        const newLocation = { latitude, longitude }
-        localStorage.setItem('userLocation', JSON.stringify(newLocation))
-      },
-      (error) => {
-        console.error('Error al obtener la ubicación:', error)
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-      }
-    )
+    const storedLocation = JSON.parse(localStorage.getItem('userLocation'))
+    if (storedLocation) {
+      return
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log('seteando nueva ubicacion')
+          const { latitude, longitude } = position.coords
+          const newLocation = { latitude, longitude }
+          localStorage.setItem('userLocation', JSON.stringify(newLocation))
+        },
+        (error) => {
+          console.error('Error al obtener la ubicación:', error)
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        }
+      )
+    }
   }
   return (
     <main
