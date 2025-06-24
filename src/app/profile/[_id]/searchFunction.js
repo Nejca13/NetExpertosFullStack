6af0b6e89ec1d593067bd478774a74652cc73b04
Nejca13 <nicolascontreras677@gmail.com>
2025-06-24 17:03:33 -0300

    Refactor searchFunction to improve filtering logic; replace filter with map for better structure and clarity

--- src/app/profile/[_id]/searchFunction.js
+++ src/app/profile/[_id]/searchFunction.js
@@ -9,20 +9,28 @@ const normalizeString = (str) => {
 
 export const searchFunction = (searchItems) => {
   const normalizedSearch = normalizeString(searchItems)
-  const result = profesiones.filter((rubro) => {
-    const rubroKey = Object.keys(rubro)[0]
-    const rubroLowerCase = normalizeString(rubroKey)
-    const profesionesArray = rubro[rubroKey]
-    const matchesRubro = rubroLowerCase.includes(normalizedSearch)
 
-    // Verificar si el nombre del rubro coincide
-    if (matchesRubro) return true
-    // Verificar si alguna de las profesiones del rubro coincide
-    const matchesProfesiones = profesionesArray.some((profesion) =>
-      normalizeString(profesion).includes(normalizedSearch)
-    )
-    return matchesProfesiones
-  })
+  const result = profesiones
+    .map((rubro) => {
+      const rubroKey = Object.keys(rubro)[0]
+      const rubroLowerCase = normalizeString(rubroKey)
+      const profesionesArray = rubro[rubroKey]
+
+      const matchesRubro = rubroLowerCase.includes(normalizedSearch)
+
+      if (matchesRubro) return { [rubroKey]: profesionesArray }
+
+      const matchedProfesiones = profesionesArray.filter((profesion) =>
+        normalizeString(profesion).includes(normalizedSearch)
+      )
+
+      if (matchedProfesiones.length > 0) {
+        return { [rubroKey]: matchedProfesiones }
+      }
+
+      return null
+    })
+    .filter(Boolean)
 
   return result
 }
