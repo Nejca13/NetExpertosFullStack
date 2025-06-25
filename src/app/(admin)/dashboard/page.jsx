83f8f82e918b0f2b42682db241fe15c9742f9886
agusasaad <agusasaad1099@hotmail.com>
2025-06-25 14:25:57 -0300

    Implement TabsComponent in DashboardPage for improved navigation and user experience

--- src/app/(admin)/dashboard/page.jsx
+++ src/app/(admin)/dashboard/page.jsx
@@ -8,21 +8,50 @@ import Profesiones from '@/components/Admin/Profesiones/Profesiones'
 import useStore from '@/store/store'
 import SideBar from '@/components/ChakraUi/SideBar/SideBar'
 import { ColorModeButton } from '@/components/ChakraUi/color-mode'
-import { Box, HStack, Stack, Text } from '@chakra-ui/react'
+import { Box, Flex, HStack, Stack, Tabs, Text } from '@chakra-ui/react'
 import { useEffect } from 'react'
+import TabsComponent from '@/components/ChakraUi/TabsComponent/TabsComponent'
+import Cliente from '@/assets/icon/Cliente'
+import Profesional from '@/assets/icon/Profesional'
+import Cita from '@/assets/icon/Cita'
+import Denuncia from '@/assets/icon/Denuncia'
+import Profesion from '@/assets/icon/Profesion'
+
+const items = [
+  {
+    key: 'clientes',
+    label: 'Clientes',
+    icon: <Cliente />,
+    component: <Clientes />,
+  },
+  {
+    key: 'profesionales',
+    label: 'Profesionales',
+    icon: <Profesional />,
+    component: <Profesionales />,
+  },
+  {
+    key: 'profesiones',
+    label: 'Profesiones',
+    icon: <Profesion />,
+    component: <Profesiones />,
+  },
+  {
+    key: 'citas',
+    label: 'Citas',
+    icon: <Cita />,
+    component: <Citas />,
+  },
+  {
+    key: 'denuncias',
+    label: 'Denuncias',
+    icon: <Denuncia />,
+    component: <Denuncias />,
+  },
+]
 
 const DashboardPage = () => {
-  const { typeSection, setTypeSection, hasHydrated } = useStore()
-
-  const componentsMap = {
-    clientes: Clientes,
-    profesionales: Profesionales,
-    profesiones: Profesiones,
-    citas: Citas,
-    denuncias: Denuncias,
-  }
-
-  const Component = componentsMap[typeSection] || null
+  const { currentUser, typeSection, setTypeSection, hasHydrated } = useStore()
 
   useEffect(() => {
     if (hasHydrated) {
@@ -30,26 +59,27 @@ const DashboardPage = () => {
     }
   }, [hasHydrated])
 
-  if (!hasHydrated) {
-    return null
-  }
+  if (!hasHydrated) return null
 
   return (
-    <Stack minH='100vh' padding='15px 20px 20px 20px'>
-      <HStack justify='space-between' align='center' mb='20px'>
-        <Text
-          textTransform='capitalize'
-          fontSize='2xl'
-          fontWeight='500'
-          mr='auto'
-        >
-          {typeSection}
-        </Text>
-        <ColorModeButton />
-        <SideBar setTypeSection={setTypeSection} />
+    <Stack padding='15px 20px 20px 20px'>
+      <HStack justify='space-between' align='center' fontWeight='600'>
+        NetExpertos Dashboard
+        <Flex>
+          <ColorModeButton />
+          <SideBar
+            currentUser={currentUser?.user_data}
+            items={items}
+            setTypeSection={setTypeSection}
+          />
+        </Flex>
       </HStack>
 
-      <Box>{Component && <Component type={typeSection} />}</Box>
+      <TabsComponent
+        value={typeSection}
+        onValueChange={setTypeSection}
+        tabs={items}
+      />
     </Stack>
   )
 }
