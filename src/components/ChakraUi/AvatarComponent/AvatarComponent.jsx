179c1723315692b5d9418fc66f252a6b02ae5e64
agusasaad <agusasaad1099@hotmail.com>
2025-06-25 14:25:51 -0300

    Enhance AvatarComponent by adding size prop and adjusting layout for improved flexibility

--- src/components/ChakraUi/AvatarComponent/AvatarComponent.jsx
+++ src/components/ChakraUi/AvatarComponent/AvatarComponent.jsx
@@ -1,12 +1,12 @@
 import { Avatar, HStack, Stack, Text } from '@chakra-ui/react'
 
-const AvatarComponent = ({ name, img, correo }) => {
+const AvatarComponent = ({ name, img, correo, size = 'md' }) => {
   return (
     <Stack gap='8'>
-      <HStack key={correo} gap='4'>
-        <Avatar.Root>
+      <HStack key={correo} gap='3'>
+        <Avatar.Root size={size}>
           <Avatar.Fallback name={name} />
-          <Avatar.Image src={img} />
+          <Avatar.Image src={img} w />
         </Avatar.Root>
         <Stack gap='0'>
           <Text fontWeight='medium'>{name}</Text>
