d41e5bae4f02c0fb0aeda7919673197085bba22c
Nejca13 <nicolascontreras677@gmail.com>
2025-06-26 10:04:29 -0300

    Add ChargeWorksPage component and associated CSS module for managing professional work submissions

---
+++ src/app/profile/profesional/charge-works/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+import CargarTrabajos from '@/components/ui/HambMenu/Perfil/CargarTrabajos/CargarTrabajos'
+import styles from './page.module.css'
+
+const ChargeWorksPage = () => {
+  return <CargarTrabajos></CargarTrabajos>
+}
+
+export default ChargeWorksPage
