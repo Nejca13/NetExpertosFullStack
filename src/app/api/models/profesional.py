375e8f805cecf8b4bc8eb33bd4bf532af1bd1210
Nejca13 <nicolascontreras677@gmail.com>
2025-06-30 10:18:09 -0300

    Refactor Profesional model by removing unused imports and cleaning up OTP fields

--- src/app/api/models/profesional.py
+++ src/app/api/models/profesional.py
@@ -1,8 +1,7 @@
-from pydantic import BaseModel
-from typing import Optional, List, Annotated
+from pydantic import BaseModel, field_validator
+from typing import Optional, List
 from datetime import datetime
 from ..models.foto import Foto
-from typing import Optional, List, Annotated
 
 
 class Profesional(BaseModel):
@@ -18,15 +17,18 @@ class Profesional(BaseModel):
     experiencia_laboral_años: int
     recomendaciones: Optional[int] = 0
     foto_perfil: str
-    fotos_trabajos: Optional[List[Foto]] = None  # Lista de objetos Foto
+    fotos_trabajos: Optional[List[Foto]] = None
     horarios_atencion: str
     nacimiento: str
     rubro_nombre: Optional[str] = None
     profesion_nombre: str
     acerca_de_mi: Optional[str] = None
     fecha_registro: Optional[datetime] = None
-    # Campos relacionados con el OTP
-    otp_code: Optional[str] = None  # Código de verificación OTP
-    otp_generated_time: Optional[datetime] = (
-        None  # Fecha y hora en que se generó el OTP
+    otp_code: Optional[str] = None
+    otp_generated_time: Optional[datetime] = None
+
+    @field_validator(
+        "nombre", "apellido", "correo", "ubicacion", "rubro_nombre", "profesion_nombre"
     )
+    def normalize_text(cls, v):
+        return v.strip().lower() if isinstance(v, str) else v
