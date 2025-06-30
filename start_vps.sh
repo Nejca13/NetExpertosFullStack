a09e52b6f2129b067a95acbdcfba57afc5d21c92
Nejca13 <nicolascontreras677@gmail.com>
2025-06-30 10:41:16 -0300

    Remove --reload option from uvicorn command in start_vps.sh

--- start_vps.sh
+++ start_vps.sh
@@ -14,4 +14,4 @@ echo -e "Dependencias de Python instaladas exitosamente."
 
 export PYTHONPATH=$(pwd)/src
 
-uvicorn src.app.api.main:app --reload --host 0.0.0.0 --port 8000
+uvicorn src.app.api.main:app --host 0.0.0.0 --port 8000
