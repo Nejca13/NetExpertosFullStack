bf26bc9f5258cdc563240a20181a243d7ebd14c6
Nejca13 <nicolascontreras677@gmail.com>
2025-07-02 16:36:49 -0300

    Add fmc router to main API and reorder router inclusions for clarity

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.core import PROFESIONES_COLLECTION
 from app.api.utils.helper_create_profesions import crear_profesiones_default
-from .routers import clientes, profesionales, profesion, citas, chat, denuncias
+from .routers import clientes, profesionales, profesion, citas, chat, denuncias, fmc
 from .security import auth_classic, auth_google
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -22,12 +22,15 @@ app.add_middleware(SessionMiddleware, secret_key="netexpertos")
 
 app.include_router(clientes.router)
 app.include_router(profesionales.router)
-app.include_router(profesion.router)
-app.include_router(auth_classic.router)
-app.include_router(auth_google.router)
-app.include_router(citas.router)
 app.include_router(chat.router)
+app.include_router(citas.router)
 app.include_router(denuncias.router)
+
+app.include_router(profesion.router)
+app.include_router(fmc.router)
+
+app.include_router(auth_google.router)
+app.include_router(auth_classic.router)
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
